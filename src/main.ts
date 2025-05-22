81813ce8974b1e6faff12ccdff1147dbd89910ab
Ulvy156 <ulvyromy156@gmail.com>
2025-05-22 09:33:38 +0700

    feat: refactor configuration management to use environment variables for database and server settings

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Load .env file
-  const configService = app.get(ConfigService);
-  const ip = configService.get<string>('IP', 'localhost'); // Default to 'localhost'
-  const port = parseInt(configService.get<string>('PORT', '3000'));
+  // Load .env properties
+  const ip = process.env.IP ?? 'localhost';
+  const port = parseInt(process.env.PORT ?? '3000');
+
   // front-end url
-  const frontEndUrl = configService.get<string>(
-    'FRONT_END_URL',
-    'http://localhost:5173',
-  );
+  const frontEndUrl = process.env.FRONT_END_URL;
+
   //log request from user
   app.useLogger(['log', 'warn', 'error']);
+
   // Enable CORS for your front-end origin
   app.enableCors({
     origin: frontEndUrl,
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: '*',
   });
+
   // Enables automatic type conversion
   app.useGlobalPipes(
     new ValidationPipe({
@@ -29,6 +29,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
   //start Nest App
   await app.listen(port, ip, () => {
     console.log(`Application is running on: http://${ip}:${port}`);
