871b43f502c1856d7f18f5b266975787a1b15340
Ulvy156 <ulvyromy156@gmail.com>
2025-06-03 15:47:11 +0700

    feat: implement JWT authentication middleware and configuration, add cookie-parser dependency

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Load .env properties
   const ip = process.env.IP ?? 'localhost';
   const port = parseInt(process.env.PORT ?? '3000');
-
+  // Enable cookie parsing
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  app.use(cookieParser());
   //log request from user
   app.useLogger(['log', 'warn', 'error']);
 
@@ -16,6 +19,7 @@ async function bootstrap() {
     origin: process.env.FRONT_END_URL,
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: '*',
+    credentials: true,
   });
 
   // Enables automatic type conversion
